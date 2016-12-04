ab060c1b4cfb373e23c81174cba12f176e4103e9
Carson Judge <cjudge1337@gmail.com>
2016-12-04 14:15:37 -0800

    Finish raisl controller intro

--- W4D1/library/app/controllers/books_controller.rb
+++ W4D1/library/app/controllers/books_controller.rb
@@ -1,18 +1,29 @@
 class BooksController < ApplicationController
   def index
     # your code here
+    @books = Book.all
   end
 
   def new
     # your code here
+    render "books/new.html.erb"
   end
 
   def create
     # your code here
+    book = Book.new(book_params)
+
+    if book.save
+      redirect_to "/books"
+    else
+      render json: book.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   def destroy
     # your code here
+    Book.find(params[:id]).delete
+    redirect_to "/books"
   end
 
   private
