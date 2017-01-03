3c72cde9f312c730adf51af5dc1025aa2ab9f4e3
Carson Judge <cjudge1337@gmail.com>
2017-01-02 20:00:02 -0800

    w7d1 finish redux_middleware

--- W7D2/redux_middleware/bundle.js
+++ W7D2/redux_middleware/bundle.js
@@ -67,30 +67,9 @@
 	document.addEventListener('DOMContentLoaded', function () {
 	  var preloadedState = localStorage.state ? JSON.parse(localStorage.state) : {};
 	  var store = (0, _store2.default)(preloadedState);
-	  store = applyMiddlewares(store, addLoggingToDispatch);
 	  var root = document.getElementById('content');
 	  _reactDom2.default.render(_react2.default.createElement(_root2.default, { store: store }), root);
 	});
-	
-	var addLoggingToDispatch = function addLoggingToDispatch(store) {
-	  return function (next) {
-	    return function (action) {
-	      console.log(store.getState());
-	      console.log(action);
-	      var result = next(action);
-	      console.log(store.getState());
-	      return result;
-	    };
-	  };
-	};
-	
-	var applyMiddlewares = function applyMiddlewares(store, middlewares) {
-	  var dispatch = store.dispatch;
-	  middlewares.forEach(function (middleware) {
-	    dispatch = middleware(store)(dispatch);
-	  });
-	  return Object.assign({}, store, { dispatch: dispatch });
-	};
 
 /***/ },
 /* 1 */
@@ -21520,11 +21499,23 @@
 	var configureStore = function configureStore() {
 	  var preloadedState = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 	
-	  var store = (0, _redux.createStore)(_root_reducer2.default, preloadedState);
+	  var store = (0, _redux.createStore)(_root_reducer2.default, preloadedState, (0, _redux.applyMiddleware)(addLoggingToDispatch));
 	  store.subscribe(function () {
 	    localStorage.state = JSON.stringify(store.getState());
 	  });
 	  return store;
+	};
+	
+	var addLoggingToDispatch = function addLoggingToDispatch(store) {
+	  return function (next) {
+	    return function (action) {
+	      console.log(store.getState());
+	      console.log(action);
+	      var result = next(action);
+	      console.log(store.getState());
+	      return result;
+	    };
+	  };
 	};
 	
 	exports.default = configureStore;
